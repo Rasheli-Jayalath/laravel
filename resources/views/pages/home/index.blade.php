10794b4906ccac03cd18affe706f9ef48e4e8cb6
Rasheli Jayalath <Rasheli@IfhamI.smeclk.com>
2023-05-23 09:03:38 +0530

    todo working files

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container text-center">
+    <div class="row ">
+      <div class="col-12 text-center pt-3">
+        <h1>Home</h1>
+      </div>
+    </div>
+  </div>
+@endsection
